9f1f06205520c32d55640450d19d0fa06d16d66b
CSI-Rolando-Baez <23-077@sanignacio.pr>
2021-10-21 10:31:24 -0400

    in-progress

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,19 +1,22 @@
-# Weapon variables
-weapon = "SV-98"
-calibre = "7.62x54mmR"
-ammoType = "BS"
-projectileSpeed = 785.0
-# Building variables
-building = "Willis Tower"
-height = 526.99
-# Separate variables
-gravity = 9.81
+import json
 
-time = (((2.0*height)/gravity) ** 0.5) # Formula for time
-print(f"The amount of time it travels for is "+ time +" seconds")
+experimentalData = {
+    'weapon': "SV-98",
+    'calibre': "7.62x54mmR",
+    'ammoType': "BS",
+    'projectileSpeed': 785.0,
+    'building': "Willis Tower",
+    'height': 526.99,
+    'gravity': 9.81
+}
 
-distance = (projectileSpeed*time) # Formula for distance
-print(f"The distance it travels is " + distance + "meters")
+
+def experimentalData.time(((2.0*height)/gravity) ** 0.5):
+print(f"The amount of time it travels for is "+ experimentalData.time +" seconds")
+
+def experimentalData.distance(projectileSpeed*time):
+
+print(f"The distance it travels is " + experimentalData.distance + "meters")
 
 # | This code looks to calculate how far a "BS" bullet shot from an SV-98 while on-top of the Willis Tower travels. The projectile speed
 # | of the bullet is 785 m/s while the height of the building is 526.99m. To find the distance the bullet travels we need to find time
