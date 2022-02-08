bb5888f7161e5fcf3d8f1c28f9b502207bbb1ed6
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-02-08 10:19:45 -0400

    web-servers project

--- Projects/Web-Servers/DeserializeRequest.py
+++ Projects/Web-Servers/DeserializeRequest.py
@@ -1,17 +1,25 @@
 import json, ssl
 import urllib.request
+import os
+from pathlib import Path
 from RandomBlood import RandomBlood
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 bloodURL = "https://random-data-api.com/api/blood/random_blood"
 
+# Create directory
+myPath = Path(__file__).parents[0]
+myFolderPath = os.path.join(myPath, 'responses')
+
 addresses:RandomBlood = [] 
 
-req = urllib.request.Request(bloodURL)
-requestData = json.loads(urllib.request.urlopen(req).read())
+for r in range(100):
 
-for r in requestData:  
-    blood:RandomBlood = RandomBlood(**r)
-    addresses.append(blood) 
-    print(blood.id)
+    req = urllib.request.Request(bloodURL)
+    requestData = json.loads(urllib.request.urlopen(req).read())
+
+    for r in requestData:  
+        blood:RandomBlood = RandomBlood(**r)
+        addresses.append(blood) 
+        print(blood.id)
