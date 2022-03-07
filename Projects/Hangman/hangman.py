0281b4e4fe559b5170799eee3e307112e07ef363
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-03-07 10:29:10 -0400

    update

--- Projects/Hangman/hangman.py
+++ Projects/Hangman/hangman.py
@@ -1,33 +1,5 @@
 import random
 
-def get_word():
-  possibleWords = ['ponce', 'guaynabo', 'fajardo', 'bayamon', 'carolina', 'cabo rojo', 'san juan', 'aguadilla', 'moca', 'lajas', 'arecibo', 'orocovis', 'cayey', 'corozal', 'salinas', 'arroyo', 'yabucoa', 'humacao', 'ceiba', 'luquillo']
-  hangmanWord = random.choice(possibleWords)
-
-get_word()
-
-def get_input(hangmanWord):
-  while True:
-    letter = input("Choose letter")
-    if letter.isalpha():
-      break
-    print("Please choose a valid Character")
-    if letter in hangmanWord:
-    print('Correct Guess!')
-    elif letter not in hangmanWord:
-    
-    
-get_input()
-
-
-
-
-def print_word(hangmanWord):
-  blank_word = len(hangmanWord) * ['_']
-  print(blank_word)
-
-print_word()
-    
 steps = ["""
  
       ___________
@@ -118,10 +90,67 @@ steps = ["""
   \ \_/ /       []
    \| |/        []
     |_|         []
-    / \        []
-   /   \       []
+    / \         []
+   /   \        []
     ==============
 
 """
 ]
-print(steps[0])
+def get_word():
+  possibleWords = ['ponce', 'guaynabo', 'fajardo', 'bayamon', 'carolina', 'cabo rojo', 'san juan', 'aguadilla', 'moca', 'lajas', 'arecibo', 'orocovis', 'cayey', 'corozal', 'salinas', 'arroyo', 'yabucoa', 'humacao', 'ceiba', 'luquillo']
+  hangmanWord = random.choice(possibleWords)
+  return hangmanWord
+
+def used_letters():
+  lettersList = []
+  maxTries = 15
+  while len(lettersList) < maxTries:
+    lettersList.append
+
+def print_word(hangmanWord):
+  blank_word = len(hangmanWord) * '_'
+  word = print(blank_word)
+  return word
+
+def get_input():
+  while True:
+    letter = input("Choose letter")
+    if letter.isalpha():
+      print("Choose a valid character (A letter from a to z)")
+      continue
+      
+    return letter
+ 
+def word_check(letter, hangmanWord, lettersList):
+      if letter not in hangmanWord:
+        print('Incorrect guess...')
+        lettersList.append(letter)
+        print (steps[0])
+        print(lettersList)
+      if letter in hangmanWord:
+        print('Correct Guess!')
+        lettersList.append(letter)
+        print(lettersList)
+
+def fill_blanks(letter, hangmanWord, lettersList):
+  temp = ""
+  for letter in hangmanWord:
+    if(letter in lettersList):
+      temp = temp + letter
+    else:
+      temp = temp + '_'
+
+get_word()
+
+print_word('hangmanWord')
+
+get_input()
+
+used_letters()
+
+word_check()
+
+fill_blanks()
+
+
+    
