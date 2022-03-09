15f864195f451e787650fe4d5986afd86369e441
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-03-09 10:16:41 -0400

    Step 3

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -2,15 +2,14 @@ import pygame
 pygame.init()
 dis=pygame.display.set_mode((400,300))
 pygame.display.update()
-# Names the created window the specified name.
 pygame.display.set_caption('Snake game by Edureka')
-# The game_over variable helps for later itirations of the code so that the game closes or restarts when lost. 
-# For now it will be used so that the window does not close
 game_over=False
-#This loop makes it so the game does not close once it's opened
 while not game_over:
     for event in pygame.event.get():
-        print(event)   #prints out all the actions that take place on the screen
+        # This line alone allows the game to actually close when prompted to, when as before the game would not close
+        # unless the Terminal itself was closed
+        if event.type==pygame.QUIT:
+            game_over=True
  
 pygame.quit()
 quit()
