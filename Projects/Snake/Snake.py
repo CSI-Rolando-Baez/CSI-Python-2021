3ec892ad09e88082aee1f97f1d708257818130c9
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-03-09 10:14:17 -0400

    Step 2

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,10 +1,16 @@
 import pygame
-# Initializes the code
 pygame.init()
-#This line of code serves to create the display for the snake game. The integers serve as the pixel size of the window opened.
 dis=pygame.display.set_mode((400,300))
-# Update will update the screen as set_mode has described
 pygame.display.update()
-# Unitializes the code
+# Names the created window the specified name.
+pygame.display.set_caption('Snake game by Edureka')
+# The game_over variable helps for later itirations of the code so that the game closes or restarts when lost. 
+# For now it will be used so that the window does not close
+game_over=False
+#This loop makes it so the game does not close once it's opened
+while not game_over:
+    for event in pygame.event.get():
+        print(event)   #prints out all the actions that take place on the screen
+ 
 pygame.quit()
 quit()
