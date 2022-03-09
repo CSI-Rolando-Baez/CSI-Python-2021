a53efdd9d8fc1b5855b64c7fae899860fcd5b44d
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-03-09 10:10:20 -0400

    Step 1

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,10 @@
+import pygame
+# Initializes the code
+pygame.init()
+#This line of code serves to create the display for the snake game. The integers serve as the pixel size of the window opened.
+dis=pygame.display.set_mode((400,300))
+# Update will update the screen as set_mode has described
+pygame.display.update()
+# Unitializes the code
+pygame.quit()
+quit()
