1b8f744989a856b1c18072cb1675fc689a090fa7
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-03-15 09:58:24 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,69 +1,95 @@
 import pygame
 import time
+import random
+ 
 pygame.init()
  
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
- # Used as a more refined code to define the size of the window in order to use it for the boundry death mechanic.
+blue = (0, 0, 255)
+ 
 dis_width = 800
-dis_height  = 600
-dis = pygame.display.set_mode((dis_width, dis_width))
+dis_height = 600
+ 
+dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Edureka')
  
-game_over = False
-# This will be used to define the current location of the snake within the screen.
-x1 = dis_width/2
-y1 = dis_height/2
- 
-snake_block=10
- # This will be used to change the direction of the snake depending on what values are asigned.
-x1_change = 0
-y1_change = 0
- 
 clock = pygame.time.Clock()
-snake_speed=30
  
-font_style = pygame.font.SysFont(None, 50)
- # Will create a certain message when called upon.
-def message(msg,color):
+snake_block = 10
+snake_speed = 30
+ 
+font_style = pygame.font.SysFont(None, 30)
+ 
+ 
+def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width/2, dis_height/2])
- # This will use button inputs to move the snake.
- # Once a button is pressed down the snake will keep going in the same direction.
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x1_change = -snake_block
-                y1_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x1_change = snake_block
-                y1_change = 0
-            elif event.key == pygame.K_UP:
-                y1_change = -snake_block
-                x1_change = 0
-            elif event.key == pygame.K_DOWN:
-                y1_change = snake_block
-                x1_change = 0
-    # Will kill the snake if it touches the boundries of the window.
-    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-        game_over = True
+    dis.blit(mesg, [dis_width/3, dis_height/3])
  
-    x1 += x1_change
-    y1 += y1_change
-    dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+ # Creates a sepparate function for the game itself
+def gameLoop():  # creating a function
+    game_over = False
+    game_close = False
  
-    pygame.display.update()
+    x1 = dis_width / 2
+    y1 = dis_height / 2
  
-    clock.tick(snake_speed)
- # Prints out a red message saying "You lost" using the message function.
-message("You lost",red)
-pygame.display.update()
-time.sleep(2)
+    x1_change = 0
+    y1_change = 0
+    # Variables for the food placement within the window.
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
  
-pygame.quit()
-quit()
+    while not game_over:
+ 
+        while game_close == True:
+            dis.fill(white)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            pygame.display.update()
+           # Lets you restart the game or close it once lost.
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+ 
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+ 
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(white)
+        # added the food being drawn within the game itself
+        pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
+        pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+        pygame.display.update()
+        # Will make it so that if the player "eats" (goes through a food piece) it will dissapear)
+        if x1 == foodx and y1 == foody:
+            print("Yummy!!")
+        clock.tick(snake_speed)
+ 
+    pygame.quit()
+    quit()
+ 
+ 
+gameLoop()
