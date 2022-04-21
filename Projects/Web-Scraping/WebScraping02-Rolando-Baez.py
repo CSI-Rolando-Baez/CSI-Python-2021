5d8714c84dac972f6e1ef2ce59667e9d96d9f4cb
CSI-Rolando-Baez <23-077@sanignacio.pr>
2022-04-21 09:55:24 -0400

    Web Scraping error fixed

--- Projects/Web-Scraping/WebScraping02-Rolando-Baez.py
+++ Projects/Web-Scraping/WebScraping02-Rolando-Baez.py
@@ -3,4 +3,4 @@ res = requests.get("https://gutenberg.org/cache/epub/67893/pg67893.txt")
 playFile = open("The Rover Boys Shipwrecked.text", "wb")
 for chunk in res.iter_content(100000):
     playFile.write(chunk)
-playFile()
+playFile.close()
